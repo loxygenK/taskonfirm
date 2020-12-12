a132c75cc38b8a18c798b24542f6c4417784b8e6
loxygenK <loxygen.k@gmail.com>
2020-12-13 02:42:38 +0900

    🏷️ Changed CheckboxContext into class and created useful constructor

--- src/lib/CheckboxContext.ts
+++ src/lib/CheckboxContext.ts
@@ -1,5 +1,15 @@
-export type CheckboxContext = {
-  checked: number,
-  unchecked: number,
-  ignored: number
-};
+import { Checkbox } from "./Checkbox";
+
+export class CheckboxContext {
+
+  readonly checked: number;
+  readonly unchecked: number;
+  readonly cancelled: number;
+
+  constructor(checkboxes: Checkbox[]) {
+    this.checked = checkboxes.filter((box) => box.state === "checked").length;
+    this.unchecked = checkboxes.filter((box) => box.state === "unchecked").length;
+    this.cancelled = checkboxes.filter((box) => box.state === "cancelled").length;
+  }
+
+}
