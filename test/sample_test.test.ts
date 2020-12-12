c332aba0bc6038c8ea727ea5cb383e523c95aaa2
loxygenK <loxygen.k@gmail.com>
2020-12-13 03:21:11 +0900

    ✅ Added a sample test

---
+++ test/sample_test.test.ts
@@ -0,0 +1,8 @@
+function isOdd(testingNumber: number): boolean {
+  return (testingNumber % 2 === 1);
+}
+
+test('Test isOdd function', () => {
+  expect(isOdd(1)).toBe(true);
+  expect(isOdd(2)).toBe(false);
+});
