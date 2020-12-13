d53ae2985db391f57342216373ebf68da323e755
loxygenK <loxygen.k@gmail.com>
2020-12-13 10:41:48 +0900

    🎨 Fixed prettier error.

--- test/sample_test.test.ts
+++ test/sample_test.test.ts
@@ -1,8 +1,8 @@
 function isOdd(testingNumber: number): boolean {
-  return (testingNumber % 2 === 1);
+  return testingNumber % 2 === 1;
 }
 
-test('Test isOdd function', () => {
+test("Test isOdd function", () => {
   expect(isOdd(1)).toBe(true);
   expect(isOdd(2)).toBe(false);
 });
